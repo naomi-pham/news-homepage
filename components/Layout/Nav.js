e052c5138f8f973c9616fcefc55bcff4035886f9
Naomi Pham <75587231+naomi-pham@users.noreply.github.com>
2022-11-22 03:19:42 +0700

    Update Nav.js

--- components/Layout/Nav.js
+++ components/Layout/Nav.js
@@ -5,13 +5,6 @@ import data from "../../data/Data";
 const Nav = () => {
   const { site, nav } = data;
 
-  const style = {
-    wrapper: `flex justify-between items-center py-8 lg:px-24`,
-    navList: `sm:flex gap-8 hidden`,
-    navListMobile: `stack text-2xl mt-24 pl-8`,
-    navMobileContainer: `sm:hidden fixed top-0 bottom-0 right-0 h-full w-8/12 bg-light z-10`,
-  };
-
   const [isExpanded, setIsExpanded] = useState(false);
 
   function toggleExpanded() {
@@ -20,17 +13,17 @@ const Nav = () => {
 
   return (
     <header>
-      <nav className={style.wrapper}>
+      <nav className="flex justify-between items-center py-8 lg:px-24">
         {/* Nav Desktop */}
         <img src={site.logo} alt="logo" />
-        <ul className={style.navList}>
+        <ul className="sm:flex gap-8 hidden">
           {nav.map((data) => (
             <li key={data.id}>
               <Link href={data.link}>{data.name}</Link>
             </li>
           ))}
         </ul>
-        {/* Nav Mobile */}
+        {/* Hamburger Icon */}
         <img
           onClick={toggleExpanded}
           src={
@@ -40,9 +33,10 @@ const Nav = () => {
           }
           className="z-20 sm:hidden"
         />
+        {/* Nav Mobile */}
         {isExpanded && (
-          <div className={style.navMobileContainer}>
-            <ul className={style.navListMobile}>
+          <div className="sm:hidden fixed top-0 bottom-0 right-0 h-full w-8/12 bg-light z-10">
+            <ul className="stack text-2xl mt-24 pl-8">
               {nav.map((data) => (
                 <li onClick={toggleExpanded} key={data.id}>
                   <Link href={data.link}>{data.name}</Link>
