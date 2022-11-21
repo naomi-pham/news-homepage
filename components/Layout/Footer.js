04b050b55dfe94d44f0f5c46554c2b690fa36249
Naomi Pham <75587231+naomi-pham@users.noreply.github.com>
2022-11-22 03:16:08 +0700

    Update Footer.js

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="[ stack ] [ ] mt-16 py-8 lg:px-24">
+    <footer className="stack mt-16 py-8 lg:px-24">
       <hr className="border-dark/10" />
       <small className="text-dark_accent text-center">
         Challenge by{" "}
