c684b7d287277f1a49ba02a9e07b54006b9609ed
Naomi Pham <75587231+naomi-pham@users.noreply.github.com>
2022-12-06 20:44:00 +0700

    Update Button.js

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -2,10 +2,8 @@ import React from "react";
 
 const Button = ({ children }) => {
   return (
-    <button className="bg-secondary py-1.5 px-2">
-      <p className="font-mono font-bold uppercase tracking-widest text-light">
+    <button className="bg-secondary py-1.5 px-2 font-mono font-bold uppercase tracking-widest text-light">
         {children}
-      </p>
     </button>
   );
 };
